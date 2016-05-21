efc96e024ef94cc6d01b128489419a46e82bf929
Michael Geissler <ibnzbssrckr@yahoo.com>
2016-05-21 15:37:51 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,28 @@
+/// <reference path="typings/globals/express/index.d.ts" />
+
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+    requireAuthentication: function(req, res, next) {
+        console.log('private route hit!');
+        next();
+    },
+    logger: function (req, res, next) {
+        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+   res.send('<h1>About Us</h1>'); 
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+    console.log('Express server started on port ' + PORT + '!');
+});
